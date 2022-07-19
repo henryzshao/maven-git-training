7b89cb1c756b404969488dbf90ba758ec8f66be1
henryzshao <henryzshao@gmail.com>
2022-07-19 13:16:03 -0500

    initial project setup

---
+++ src/main/java/com/hcl/henryshao/main/MainClass.java
@@ -0,0 +1,12 @@
+package com.hcl.henryshao.main;
+
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Hello World " + i);
+		}
+	}
+
+}
